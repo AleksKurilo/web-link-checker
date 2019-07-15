374f1fe10fbfbd487ba71bb321186d51c35c64cf
aleksk <aleks_prime@mail.ru>
2019-07-15 21:51:39 +0300

    initializing.

---
+++ src/main/java/com/web/weblinkchecker/WeblinkcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.web.weblinkchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblinkcheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblinkcheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/weblinkchecker/WeblinkcheckerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.web.weblinkchecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeblinkcheckerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        // need by default
+    }
+
+}
